5128eb650c3e67177adb608bbf092678077e682a
denialmedia <58999891+denialmedia@users.noreply.github.com>
2021-03-24 08:57:58 -0500

    First Commit

---
+++ Scripts/main.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+onready var anim = get_node("AnimationPlayer")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	anim.play("drip")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
